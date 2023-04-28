2b758eeecfea79746beea94b47a2fce3c3e5d414
ShivaniF2000 <119884719+ShivaniF2000@users.noreply.github.com>
2023-04-28 13:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+	console.log(str.length)
 };
-
+)
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
